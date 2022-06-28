ec8a5e957e4ad82efac73a938fa8a4143731ff41
KHAN KHAN <104531478+MuhammadKhankhan@users.noreply.github.com>
2022-06-28 19:30:39 +0500

    Update Bypass.py

--- Bypass.py
+++ Bypass.py
@@ -6,8 +6,6 @@ if b == '64bit':
 
     print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
 
-    os.system('xdg-open https://youtube.com/channel/UC6OSDzf3I3Ws8onwga637uA/');time.sleep(5)
-
     import SSB
 
 elif b == '32bit':
