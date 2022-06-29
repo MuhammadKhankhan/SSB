6647aa4d22a92106f15243ea9c549206e97fa949
KHAN KHAN <104531478+MuhammadKhankhan@users.noreply.github.com>
2022-06-29 09:10:39 +0500

    Update SSB.py

--- SSB.py
+++ SSB.py
@@ -4,8 +4,10 @@ b = platform.architecture()[0]
 
 if b == '64bit':
 
-import SSB
+    print("\n\x1b[1;92mCongratulations Your Device Support This Tool\033[1;37m")
+
+    import SSB
 
 elif b == '32bit':
 
-print("\x1b[1;91mOpps Sorry Beb Your Mobile Not Support This Tool")
+    print("\x1b[1;91mOpps Sorry Beb Your Mobile Not Support This Tool")
